842efdc7d391813fb6e215b266e96021d360204c
prajwalap <praj.p98@gmail.com>
2025-09-02 22:36:14 +0530

    syntax

---
+++ JavaScript/4_syntax.js
@@ -0,0 +1,25 @@
+// How to Declare variables:
+let x = 5;
+let y = 6;
+
+// How to Compute values:
+let z = x + y;
+
+// I am a Comment. I do Nothing
+
+// Define x as a variable
+let x1;
+
+// Assign the value 6 to x
+x1 = 6;
+
+//Identifiers are used to name variables and keywords, and functions.
+
+//JavaScript keywords are used to defines actions to be performed.
+let x2 = 5;
+const fname = "John";
+
+//JavaScript assignment operators (=) assign values to variables
+//JavaScript uses arithmetic operators ( + - * / ) to compute values
+
+//All JavaScript identifiers are case sensitive.
