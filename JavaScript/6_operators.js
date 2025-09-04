6b6b56b76a9ee6d5a775b69ba6f11753c4c08adf
prajwalap <praj.p98@gmail.com>
2025-09-04 17:19:46 +0530

    operators

---
+++ JavaScript/6_operators.js
@@ -0,0 +1,81 @@
+//Assignment Operators
+let x = 10;
+let y = 5;
+x = y;
+console.log("x = y");
+console.log("x = "+x+", y = "+y);
+
+x = 10;
+y = 5;
+x += y;
+console.log("x += y");
+console.log("x = "+x+", y = "+y);
+
+x = 10;
+y = 5;
+x -= y;
+console.log("x -= y");
+console.log("x = "+x+", y = "+y);
+
+x = 10;
+y = 5;
+x *= y;
+console.log("x *= y");
+console.log("x = "+x+", y = "+y);
+
+x = 10;
+y = 5;
+x /= y;
+console.log("x /= y");
+console.log("x = "+x+", y = "+y);
+
+x = 10;
+y = 5;
+x %= y;
+console.log("x %= y");
+console.log("x = "+x+", y = "+y);
+
+//The Spread (...) Operator
+//The ... operator splits iterables into individual elements.
+const numbers = [23,55,21,87,56];
+let minValue = Math.min(...numbers);
+let maxValue = Math.max(...numbers);
+console.log("Min "+minValue+" Max "+maxValue);
+
+
+//Arithmetic Operators
+x = 10;
+y = 5;
+x += y;
+console.log("x += y");
+console.log("x = "+x+", y = "+y);
+
+x = 10;
+y = 5;
+x += y;
+console.log("x += y");
+console.log("x = "+x+", y = "+y);
+
+x = 10;
+y = 5;
+x += y;
+console.log("x += y");
+console.log("x = "+x+", y = "+y);
+
+x = 10;
+y = 5;
+x += y;
+console.log("x += y");
+console.log("x = "+x+", y = "+y);
+
+x = 10;
+y = 5;
+x += y;
+console.log("x += y");
+console.log("x = "+x+", y = "+y);
+
+x = 10;
+y = 5;
+x += y;
+console.log("x += y");
+console.log("x = "+x+", y = "+y);
