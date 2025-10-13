a27223fc56a762d7e05157b0af7656f0ce412a36
prajwalap <praj.p98@gmail.com>
2025-10-13 20:55:11 +0530

    Objects contd

--- JavaScript/11_objects.js
+++ JavaScript/11_objects.js
@@ -47,3 +47,18 @@ const person3 = {
   };
 console.log("Full name is "+person3.fullName());
 //In the example above, this refers to the person object
+
+//Displaying Object Properties
+let text = person.name + "," + person.age + "," + person.city;
+
+//Object Constructor Functions
+function Person(first, last, age, eye) {
+  this.firstName = first;
+  this.lastName = last;
+  this.age = age;
+  this.eyeColor = eye;
+}
+const myFather = new Person("John", "Doe", 50, "blue");
+const myMother = new Person("Sally", "Rally", 48, "green");
+const mySister = new Person("Anna", "Rally", 18, "green");
+const mySelf = new Person("Johnny", "Rally", 22, "green");
